b0b4ce2b192a56ee8cb74bacf1db92be75c518ce
Jan Spacek <janspacek56@gmail.com>
2022-04-15 21:32:06 +0200

    (in)famous site theme

--- gui.py
+++ gui.py
@@ -71,86 +71,86 @@ text_box.tag_config('default',background='#DDDBDB', foreground="#000000")
 text_box.config(state='disabled')
 
 #row 1
-button_f10 = Button(canvas, text = "f0", height = 2, width= 3, relief=FLAT,bd=2,activebackground='#ec058e', fg='#292929', bg='#37b0c9',highlightbackground="#37b0c9", highlightthickness=1)
+button_f10 = Button(canvas, text = "f0", height = 2, width= 3, relief=FLAT,bd=2,activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1)
 button_f10.grid(row = 1, column = 0)
 
-button_f11 = Button(canvas, text = "f1", height = 2, width = 3, relief=FLAT,bd=2,fg='#292929', bg='#37b0c9',highlightbackground="#37b0c9", highlightthickness=1)
+button_f11 = Button(canvas, text = "f1", height = 2, width = 3, relief=FLAT,bd=2,activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1)
 button_f11.grid(row = 1, column = 1)
 
-button_f12 = Button(canvas, text = "f2", height = 2, width = 3, relief=FLAT,bd=2, fg='#292929', bg='#37b0c9',highlightbackground="#37b0c9", highlightthickness=1)
+button_f12 = Button(canvas, text = "f2", height = 2, width = 3, relief=FLAT,bd=2, activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1)
 button_f12.grid(row = 1, column = 2)
 
-button_clear = Button(canvas, text = "C", height = 2, width = 3, relief=FLAT,bd=2, fg='#292929', bg='#37b0c9', highlightbackground="#37b0c9", highlightthickness=1, command = lambda: clear())
+button_clear = Button(canvas, text = "C", height = 2, width = 3, relief=FLAT,bd=2,activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1, command = lambda: clear())
 button_clear.grid(row = 1, column = 3)
 
 #row 2
 #todo change
-button_root = Button(canvas, text = "root", height = 2, width= 3, relief=FLAT,bd=2, fg='#292929', bg='#37b0c9', highlightbackground="#37b0c9", highlightthickness=1)
+button_root = Button(canvas, text = "root", height = 2, width= 3, relief=FLAT,bd=2,activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1)
 button_root.grid(row = 2, column = 0)
 #todo decide what should be displayed
-button_power = Button(canvas, text = "^", height = 2, width = 3, relief=FLAT,bd=2, fg='#292929', bg='#37b0c9',  highlightbackground="#37b0c9", highlightthickness=1,command=lambda: send(str("^")))
+button_power = Button(canvas, text = "^", height = 2, width = 3, relief=FLAT,bd=2, activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1,command=lambda: send(str("^")))
 button_power.grid(row = 2, column = 1)
 
-button_factorial = Button(canvas, text = "x!", height = 2, width = 3, relief=FLAT,bd=2,fg='#292929', bg='#37b0c9', highlightbackground="#37b0c9", highlightthickness=1, command= lambda: send(str("!")))
+button_factorial = Button(canvas, text = "x!", height = 2, width = 3, relief=FLAT,bd=2,activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1, command= lambda: send(str("!")))
 button_factorial.grid(row = 2, column = 2)
 
-button_division = Button(canvas, text = "/", height = 2, width = 3, relief=FLAT,bd=2, fg='#292929', bg='#37b0c9',  highlightbackground="#37b0c9", highlightthickness=1)
+button_division = Button(canvas, text = "/", height = 2, width = 3, relief=FLAT,bd=2,activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1)
 button_division.grid(row = 2, column = 3)
 
 #row 3
 
-button_7 = Button(canvas, text = "7", height = 2, width= 3, relief=FLAT,bd=2, bg='#ec058e', fg='#1b1b1b', highlightbackground="#ec058e", highlightthickness=1, command=lambda: send(int(7)))
+button_7 = Button(canvas, text = "7", height = 2, width= 3, relief=FLAT,bd=2,activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1, command=lambda: send(int(7)))
 button_7.grid(row = 3, column = 0)
 
-button_8 = Button(canvas, text = "8", height = 2, width = 3, relief=FLAT,bd=2, bg='#ec058e', fg='#1b1b1b', highlightbackground="#ec058e", highlightthickness=1, command=lambda: send(int(8)))
+button_8 = Button(canvas, text = "8", height = 2, width = 3, relief=FLAT,bd=2, activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1, command=lambda: send(int(8)))
 button_8.grid(row = 3, column = 1)
 
-button_9 = Button(canvas, text = "9", height = 2, width = 3, relief=FLAT,bd=2, bg='#ec058e', fg='#1b1b1b', highlightbackground="#ec058e", highlightthickness=1, command=lambda: send(int(9)))
+button_9 = Button(canvas, text = "9", height = 2, width = 3, relief=FLAT,bd=2, activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1, command=lambda: send(int(9)))
 button_9.grid(row = 3, column = 2)
 
-button_mul = Button(canvas, text = "*", height = 2, width = 3,relief=FLAT,bd=2, fg='#1b1b1b', bg='#37b0c9',  highlightbackground="#37b0c9", highlightthickness=1,command = lambda: send(str("*")))
+button_mul = Button(canvas, text = "*", height = 2, width = 3,relief=FLAT,bd=2, activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1,command = lambda: send(str("*")))
 button_mul.grid(row = 3, column = 3)
 
 #row 4
-button_4 = Button(canvas, text = "4", height = 2, width= 3 ,relief=FLAT,bd=2, bg='#ec058e', fg='#1b1b1b',  highlightbackground="#ec058e", highlightthickness=1,command = lambda: send(int(4)))
+button_4 = Button(canvas, text = "4", height = 2, width= 3 ,relief=FLAT,bd=2, activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1,command = lambda: send(int(4)))
 button_4.grid(row = 4, column = 0)
 #button_1f.pack()
 
-button_5 = Button(canvas, text = "5", height = 2, width = 3 ,relief=FLAT,bd=2, bg='#ec058e', fg='#1b1b1b',  highlightbackground="#ec058e", highlightthickness=1, command=lambda: send(int(5)))
+button_5 = Button(canvas, text = "5", height = 2, width = 3 ,relief=FLAT,bd=2,activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1, command=lambda: send(int(5)))
 button_5.grid(row = 4, column = 1)
 #button_2f.pack()
 
-button_6 = Button(canvas, text = "6", height = 2, width = 3,relief=FLAT,bd=2, bg='#ec058e', fg='#1b1b1b', highlightbackground="#ec058e", highlightthickness=1, command=lambda: send(int(6)))
+button_6 = Button(canvas, text = "6", height = 2, width = 3,relief=FLAT,bd=2, activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1, command=lambda: send(int(6)))
 button_6.grid(row = 4, column = 2)
 
-button_minus = Button(canvas, text = "-", height = 2, width = 3, relief=FLAT,bd=2, fg='#1b1b1b', bg='#37b0c9',  highlightbackground="#37b0c9", highlightthickness=1, command= lambda: send(str("-")))
+button_minus = Button(canvas, text = "-", height = 2, width = 3, relief=FLAT,bd=2,activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1, command= lambda: send(str("-")))
 button_minus.grid(row = 4, column = 3)
 
 #row 5
-button_1 = Button(canvas, text = "1", height = 2, width= 3,relief=FLAT,bd=2, bg='#ec058e', fg='#1b1b1b',  highlightbackground="#ec058e", highlightthickness=1, command=lambda: send(int(1)))
+button_1 = Button(canvas, text = "1", height = 2, width= 3,relief=FLAT,bd=2, activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1, command=lambda: send(int(1)))
 button_1.grid(row = 5, column = 0)
 
-button_2 = Button(canvas, text = "2", height = 2, width = 3,relief=FLAT,bd=2, bg='#ec058e', fg='#1b1b1b',  highlightbackground="#ec058e", highlightthickness=1, command=lambda: send(int(2)))
+button_2 = Button(canvas, text = "2", height = 2, width = 3,relief=FLAT,bd=2, activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1, command=lambda: send(int(2)))
 button_2.grid(row = 5, column = 1)
 
-button_3 = Button(canvas, text = "3", height = 2, width = 3,relief=FLAT,bd=2, bg='#ec058e', fg='#1b1b1b',  highlightbackground="#ec058e", highlightthickness=1, command=lambda: send(int(3)))
+button_3 = Button(canvas, text = "3", height = 2, width = 3,relief=FLAT,bd=2, activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1, command=lambda: send(int(3)))
 button_3.grid(row = 5, column = 2)
 
-button_plus = Button(canvas, text = "+", height = 2, width = 3,relief=FLAT,bd=2, bg='#37b0c9', fg='#1b1b1b', highlightbackground="#37b0c9", highlightthickness=1, command= lambda: send(str("+")))
+button_plus = Button(canvas, text = "+", height = 2, width = 3,relief=FLAT,bd=2, activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1, command= lambda: send(str("+")))
 button_plus.grid(row = 5, column = 3)
 
 #row 6
-button_comma = Button(canvas, text = ",", height = 2, width= 3, bd=2, bg='#ec058e', fg='#1b1b1b', relief=FLAT,  highlightbackground="#ec058e", highlightthickness=1)
+button_comma = Button(canvas, text = ",", height = 2, width= 3,relief=FLAT, bd=2, activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1)
 button_comma.grid(row = 6, column = 0)
 
-button_0 = Button(canvas, text = "0", height = 2, width = 3,relief=FLAT,bd=2, bg='#ec058e', fg='#1b1b1b', highlightbackground="#ec058e", highlightthickness=1, command=lambda: send(int(0)))
+button_0 = Button(canvas, text = "0", height = 2, width = 3,relief=FLAT,bd=2,activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1, command=lambda: send(int(0)))
 button_0.grid(row = 6, column = 1)
 
-button_equal = Button(canvas, text = "=", height = 2, width = 3,relief=FLAT,bd=2, bg='#ec058e', fg='#1b1b1b',  highlightbackground="#ec058e", highlightthickness=1,command = lambda: equal())
+button_equal = Button(canvas, text = "=", height = 2, width = 3,relief=FLAT,bd=2, activebackground='#ffa31a',activeforeground="#292929", fg='#ffa31a', bg='#292929',highlightbackground="#292929", highlightthickness=1,command = lambda: equal())
 button_equal.grid(row = 6, column = 2)
 #button_equal.pack()
 
-button_f63 = Button(canvas, text = "f1", height = 2, width = 3,relief=FLAT,bd=2, bg='#37b0c9', fg='#1b1b1b',  highlightbackground="#37b0c9", highlightthickness=1)
+button_f63 = Button(canvas, text = "f1", height = 2, width = 3,relief=FLAT,bd=2, activebackground='#292929',activeforeground="#ffa31a", fg='#292929', bg='#ffa31a',highlightbackground="#ffa31a", highlightthickness=1)
 button_f63.grid(row = 6, column = 3)
 
 
